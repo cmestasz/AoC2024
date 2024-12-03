006c05c1cbbe05872740ac707e04ef3cec2b11b3
Christian <cmestasz@unsa.edu.pe>
2024-12-03 10:50:09 -0500

    4! (probably cleaner as well)

--- 3a.py
+++ 3a.py
@@ -1,10 +1,9 @@
 import re
 with open("3i.txt") as f:
     i = f.read()
-ms = re.findall(r"(mul|do|don't)\((|\d+,\d+\))", i)
+ms = re.findall(r"mul\((\d+,\d+)\)", i)
 s = 0
-e = True
 for m in ms:
-    a, b = map(int, re.findall(r"\d+", m))
+    a, b = [int(x) for x in m.split(",")]
     s += a * b
 print(s)
