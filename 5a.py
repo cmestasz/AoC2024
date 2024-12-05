570bfa0eba7cdd6b09cd5a6159b6e6543420c363
Christian <cmestasz@unsa.edu.pe>
2024-12-05 08:40:18 -0500

    5!

---
+++ 5a.py
@@ -0,0 +1,21 @@
+with open("5i.txt") as f:
+    l = f.readline().strip()
+    d = ()
+    while l != "":
+        a, b = l.split("|")
+        d += ((a, b),)
+        l = f.readline().strip()
+    l = f.readline().strip()
+    s = 0
+    while l != "":
+        c = l.split(",")
+        v = True
+        for i in range(len(c)):
+            for j in range(i, len(c)):
+                if (c[j], c[i]) in d:
+                    v = False
+                    break
+        if v:
+            s += int(c[len(c) // 2])
+        l = f.readline().strip()
+    print(s)
