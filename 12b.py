79514d4339a1e3eda33f76a60756476dcdcd5c7e
Christian <cmestasz@unsa.edu.pe>
2024-12-13 09:13:52 -0500

    math incarnate

--- 12b.py
+++ 12b.py
@@ -25,6 +25,7 @@ def ss(t, m, co, v, l, i, j):
         co.add((i, j, i, j - 1))
     return c
 
+
 with open("12i.txt") as f:
     m = [li.strip() for li in f]
     l = len(m)
@@ -35,41 +36,32 @@ for i in range(l):
     for j in range(l):
         if v[i][j]:
             continue
-        print(m[i][j])
         co = set()
         lu = ss(m[i][j], m, co, v, l, i, j)
-        lc = len(co)
         dh = {}
         dv = {}
-        for c in co:
-            if c[1] == c[3]:                
-                k1 = (c[0], c[2])
-                if k1 not in dh:
-                    dh[k1] = set()
-                    dh[k1].add(c[1])
-                    print(f"h side init ({k1}), from ({c[0]}, {c[1]}) to ({c[2]}, {c[3]})")
-                else:
-                    e = dh[k1]
-                    if (k1[0], c[1]-1, k1[1], c[1]-1) in co or (k1[0], c[1]+1, k1[1], c[1]+1) in co:
-                        lc -= 1
-                        print(f"h wall ({k1}), from ({c[0]}, {c[1]}) to ({c[2]}, {c[3]}) is already a side")
-                    else:
-                        e.add(c[1])
-                        print(f"dup code ({k1}), new h side, from ({c[0]}, {c[1]}) to ({c[2]}, {c[3]})")
-            elif c[0] == c[2]:
-                k2 = (c[1], c[3])
-                if k2 not in dv:
-                    dv[k2] = set()
-                    dv[k2].add(c[0])
-                    print(f"v side init ({k2}), from ({c[0]}, {c[1]}) to ({c[2]}, {c[3]})")
-                else:
-                    e = dv[k2]
-                    if (c[0]-1, k2[0], c[0]-1, k2[1]) in co or (c[0]+1, k2[0], c[0]+1, k2[1]) in co:
-                        lc -= 1
-                        print(f"v wall ({k2}), from ({c[0]}, {c[1]}) to ({c[2]}, {c[3]}) is already a side")
-                    else:
-                        e.add(c[0])
-                        print(f"dup code ({k2}), new v side, from ({c[0]}, {c[1]}) to ({c[2]}, {c[3]})")
+        lc = 0
+        while len(co) > 0:
+            c = co.pop()
+            lc += 1
+            if c[0] == c[2]:
+                k = (c[0] - 1, c[1], c[2] - 1, c[3])
+                while k in co:
+                    co.remove(k)
+                    k = (k[0] - 1, k[1], k[2] - 1, k[3])
+                k = (c[0] + 1, c[1], c[2] + 1, c[3])
+                while k in co:
+                    co.remove(k)
+                    k = (k[0] + 1, k[1], k[2] + 1, k[3])
+            elif c[1] == c[3]:
+                k = (c[0], c[1] - 1, c[2], c[3] - 1)
+                while k in co:
+                    co.remove(k)
+                    k = (k[0], k[1] - 1, k[2], k[3] - 1)
+                k = (c[0], c[1] + 1, c[2], c[3] + 1)
+                while k in co:
+                    co.remove(k)
+                    k = (k[0], k[1] + 1, k[2], k[3] + 1)
         cc += lu * lc
 
 print(cc)
